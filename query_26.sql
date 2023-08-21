1391a74b2a34322f565645dd34a8eaaa2a1251f7
denizcantasdelenn <136730167+denizcantasdelenn@users.noreply.github.com>
2023-08-21 12:02:43 +0300

    Add files via upload

---
+++ query_26.sql
@@ -0,0 +1,30 @@
+--find total number of messages exchanged between each person per day.
+
+CREATE TABLE subscriber (
+ sms_date date ,
+ sender varchar(20) ,
+ receiver varchar(20) ,
+ sms_no int
+);
+-- insert some values
+INSERT INTO subscriber VALUES ('2020-4-1', 'Avinash', 'Vibhor',10);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Vibhor', 'Avinash',20);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Avinash', 'Pawan',30);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Pawan', 'Avinash',20);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Vibhor', 'Pawan',5);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Pawan', 'Vibhor',8);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Vibhor', 'Deepak',50);
+
+
+--select * from subscriber
+
+
+with cte as (
+select *, 
+case when sender < receiver then sender else receiver end as first, 
+case when sender > receiver then sender else receiver end as second
+from subscriber)
+
+select sms_date, first, second, sum(sms_no)
+from cte
+group by sms_date, first, second
